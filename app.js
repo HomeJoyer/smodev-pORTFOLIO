e55e00a059adfc780c0ad2240d6d5192722f689c
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2020-02-18 15:25:08 +0000

    Updating makefile

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const morgan = require('morgan');
 const layout = require('./views/layout.js');
 const models = require('./models');
 const wikiRouter = require('./routes/wiki.js');
-const userRouter = re
+const userRouter = require('./routes/user.js');
+const mainPage = require('./views/main.js');
+const pages = require('./views/index.js');
+
+app.use(morgan('dev'))

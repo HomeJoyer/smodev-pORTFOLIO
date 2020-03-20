cbb2b7ccf221f81ffd8c72c4d873ea708d6fd53a
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2020-03-20 06:20:18 +0000

    fixing license

--- models/index.js
+++ models/index.js
@@ -3,4 +3,11 @@ const db = new Sequelize('postgres://localhost:5432/wikistack', {
   logging: false,
 });
 
-const Page = db
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull:

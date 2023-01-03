dc3d1c222e13b072fc48e55080ed5a70adca9aac
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-01-03 06:52:07 +0000

    Let CVS be quieter

--- routes/wiki.js
+++ routes/wiki.js
@@ -21,3 +21,22 @@ router.get('/:slug', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
+  // STUDENT ASSIGNMENT:
+  // add definitions for `title` and `content`
+
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise.
+  try {
+    await page.save();
+    res.redirect(`/wiki/${page.slug}`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;

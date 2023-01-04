df22152d017631c827b5022257d6a0904cb2e900
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-01-04 20:40:59 +0000

    as of today

--- views/addPage.js
+++ views/addPage.js
@@ -6,4 +6,6 @@ module.exports = () =>
     <h3>Add a Page</h3>
     <hr />
     <form method="POST" action="/wiki/">
-      <div clas
+      <div class="form-group">
+        <label for="author" class="col-sm-2 control-label">Author's Name</label>
+        <div class

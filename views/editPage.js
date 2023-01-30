ff84e9552871131b4ba8ae44a415f3553b2aef3c
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-01-30 21:35:38 +0000

    fix some links

--- views/editPage.js
+++ views/editPage.js
@@ -2,4 +2,10 @@ const html = require("html-template-tag");
 const layout = require("./layout");
 
 module.exports = (page, author) => layout(html`
-  <h3>Edit a Page</
+  <h3>Edit a Page</h3>
+  <hr>
+  <form method="POST" action="/wiki/${page.slug}">
+
+    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
+    
+    <div

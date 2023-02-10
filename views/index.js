2032e1145fa3be4d60330a4f5e6d744b5c032e53
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-02-10 15:02:49 +0000

    Remove legacy code.

--- views/index.js
+++ views/index.js
@@ -2,4 +2,7 @@ const addPage = require("./addPage");
 const editPage = require("./editPage");
 const main = require("./main");
 const userList = require("./userList");
-const userPages = require("./userPa
+const userPages = require("./userPages");
+const wikiPage = require("./wikiPage");
+
+module.exports = { addPage, editPage, main, userList, u

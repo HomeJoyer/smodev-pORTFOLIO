bb9371c7524b4c7ff5ef4b1e2f54325f3a5fba2a
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-02-11 12:10:56 +0000

    Ignore build dir

--- views/layout.js
+++ views/layout.js
@@ -1,4 +1,7 @@
 const html = require("html-template-tag");
 
 module.exports = (content) => html`<!DOCTYPE html>
-  
+  <html lang="en">
+    <head>
+      <title>WikiStack</title>
+      <link href="http://netdna.bootstrapcdn.c

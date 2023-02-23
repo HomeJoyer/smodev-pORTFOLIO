d7322ee61fce2b054be15eb895726d9fa70c7b25
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-02-23 20:20:10 +0000

    Update installation_unsigned.md

--- views/main.js
+++ views/main.js
@@ -1,1 +1,8 @@
-const html = require('html-template-t
+const html = require('html-template-tag');
+const layout = require('./layout');
+
+module.exports = pages =>
+  layout(html`
+    <h3>Pages</h3>
+    <hr />
+  

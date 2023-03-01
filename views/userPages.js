47dbaccc7b4db46129a9c4ece3d990d7706677da
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-03-01 06:03:20 +0000

    Remove executable

--- views/userPages.js
+++ views/userPages.js
@@ -4,4 +4,6 @@ const layout = require("./layout");
 module.exports = (user, pages) => layout(html`
   <h3>Pages written by ${user.name}</h3>
   <hr>
-  <form method
+  <form method="GET" action="/wiki/search">
+    <input type="text" name="search" />
+    <button type="submit">Search</butt

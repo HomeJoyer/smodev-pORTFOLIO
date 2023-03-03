fc31113165b700a88c4bbfa45268d07e096aef73
HomeJoyer <108831769+HomeJoyer@users.noreply.github.com>
2023-03-03 23:05:21 +0000

    Added sample code

--- views/wikipage.js
+++ views/wikipage.js
@@ -3,4 +3,7 @@ const layout = require("./layout");
 
 module.exports = (page, author) => layout(html`
   <h3>${page.title}
-      <small> (<a href="/wiki/${page.
+      <small> (<a href="/wiki/${page.slug}/similar">Similar</a>)</small>
+  </h3>
+  <h4>by <a href="PLACEHOLDER-AUTHOR-URL">PLACEHOLDER AUTHOR NAME</a></h4>
+  <hr/
